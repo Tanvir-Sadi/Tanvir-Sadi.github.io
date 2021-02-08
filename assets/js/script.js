2084c035476554de57dbf7eb423df6b253fed337
tanvir-sadi <tanvir.sadi10@northsouth.edu>
2021-02-08 19:06:31 +0600

    JS Function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 // slider-range
 
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+var output = document.getElementById("passwordDigit");
+output.value = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  output.value = this.value;
+}
+
+output = document.getElementById("passwordDigit");
+// Update the current slider value (each time you drag the slider handle)
+output.oninput = function() {
+  slider.value = this.value;
 }
